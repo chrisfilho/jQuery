88dffe87ae387f661c27d16b31df421e30c83d72
Christian Soares <chrisfilho19@gmail.com>
2015-08-25 20:57:29 -0300

    usando addClass

---
+++ addClass/script.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {    
+    $('#bola1').mouseenter(function() {
+        $(this).addClass('bola-azul');
+    }); 
+    
+});
+$(document).ready(function() {    
+    $('#bola2').mouseenter(function() {
+        $(this).addClass('bola-roxa');
+    }); 
+    
+});
+$(document).ready(function() {    
+    $('#bola3').mouseenter(function() {
+        $(this).addClass('bola-amarela');
+    }); 
+    
+});
+
+
+

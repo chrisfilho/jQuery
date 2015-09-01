9c95ee946f2f8b68505309c8fa1e8e7c2cf4ba3d
Christian Soares <chrisfilho19@gmail.com>
2015-09-01 19:16:46 -0300

    utilizando button

---
+++ button/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.red').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.blue').fadeOut('slow');
+    });
+});

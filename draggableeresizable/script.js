ce20167a841ac441d81cb96d0ece6f851851a51b
Christian Soares <chrisfilho19@gmail.com>
2015-09-28 21:09:25 -0300

    resizable e draggable

---
+++ draggableeresizable/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+$('#resi').click(function(){
+$('#resi').resizable();
+
+});
+
+});
+
+
+$(document).ready(function(){
+$('#drag').click(function(){
+$('#drag').draggable();
+
+});
+
+});

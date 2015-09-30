809c81d3471b89f5ab6b5d30a1143964ecf03c71
Christian Soares <chrisfilho19@gmail.com>
2015-09-30 20:08:59 -0300

    usando seletores compostos

---
+++ seletores-compostos/script.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.red').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.blue').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.pink').fadeOut('slow');
+    });
+});
+
+$(document).ready(function() {
+    $('button').click(function() {
+        $('.green').fadeOut('slow');
+    });
+});
+$(document).ready(function() {
+    $('button').dblclick(function() {
+        $('img').fadeOut('slow');
+    });
+});
